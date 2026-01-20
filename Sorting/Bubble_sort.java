6ecc16043118b495df48f77de02a5f1ba24b58f1
ankittroy-21 <oxilic05701@gmail.com>
2026-01-20 08:50:23 +0530

    Optimize Bubble Sort implementation by adding early exit condition

--- Sorting/Bubble_sort.java
+++ Sorting/Bubble_sort.java
@@ -1,17 +1,18 @@
-package Sorting;
-
 import java.util.*;
 
 public class Bubble_sort {
     public static void sort(int arr[],int n){
         for(int i =n-1;i>=1;i--){
+            int swap=0;
             for(int j=0;j<i;j++){
                 if(arr[j]>arr[j+1]){
                     int temp = arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    swap=1;
                 }
             }
+            if(swap==0) break;
         }
     }
     public static void main(String[] args) {
