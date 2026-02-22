2cd90426e47ee263f7229d81d2a142982700c073
Ankit Roy <82595330+ankittroy-21@users.noreply.github.com>
2026-02-22 10:38:44 +0530

    Refactor countApplesAndOranges method

--- Problems/Apple_orange_problem.java
+++ Problems/Apple_orange_problem.java
@@ -1,14 +1,15 @@
 public static void countApplesAndOranges(int s, int t, int a, int b, List<Integer> apples, List<Integer> oranges) {
     // Write your code here
-    int app = apples.size();
-    int org = oranges.size();
-    for (int i =0;i<app;i++){
-        if(s<=apples.get(i)+a && apples.get(i)+a<=t){
-            System.out.println("1");
-        }
+    int app = 0;
+    int org = 0;
+    for(int d:apples){
+        if(s<=a+d && a+d<=t) app++;
     }
-    for (int i =0;i<org;i++){
-        if(s<=oranges.get(i)+b && oranges.get(i)+b<=t){
-            System.out.println("1");
-        }
+    for(int d:oranges){
+        if(s<=b+d && b+d<=t) org++;
     }
+    
+    System.out.println(app);
+    System.out.println(org);
+
+}
