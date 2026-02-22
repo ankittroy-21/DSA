3f603d0b0d8f371fb0b8227d1301d451af6a0b9b
Ankit Roy <82595330+ankittroy-21@users.noreply.github.com>
2026-02-22 11:13:21 +0530

    Implement kangaroo function for number line jump

---
+++ Problems/Number_line_Jump.java
@@ -0,0 +1,6 @@
+public static String kangaroo(int x1, int v1, int x2, int v2) {
+    // Write your code here
+    if (v2 >=v1) return "NO";
+    if((x2-x1)%(v1-v2)==0) return "YES";
+    else return "NO";
+}
